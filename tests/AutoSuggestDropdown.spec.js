73ead0969826bb5d5a0c3d20dd39743dcb80f1ba
washma2906 <washmamalik2906@gmail.com>
2025-11-13 15:33:25 +0500

    auto suggest dropdown added

--- tests/AutoSuggestDropdown.spec.js
+++ tests/AutoSuggestDropdown.spec.js
@@ -1,7 +1,21 @@
 import{test, expect} from '@playwright/test';
 test.describe('Auto suggest Dropdown test suite',() =>{
-    test('auot suggestdropdown test1', async({page}) =>{
+    test('auto suggestdropdown test1', async({page}) =>{
         await page.goto('https://www.redbus.in/');
-        
+        //enter the value in the source text box
+       await page.getByRole('button',{name:'From'}).click();
+       await page.getByRole('textbox',{name:'From'}).fill('Hyderabad');
+       await page.waitForTimeout(2000);
+       await page.keyboard.press('ArrowDown');
+       await page.waitForTimeout(2000);
+       await page.keyboard.press('ArrowDown');
+       await page.waitForTimeout(2000);
+       await page.keyboard.press('Enter');
+       
+    // await page.pause();
+
+    //    await page.getByRole('button',{name:'From Search suggestions'}).click(); 
+        await page.waitForTimeout(2000);
+
     });
 })
